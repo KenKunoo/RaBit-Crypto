bae62c31881b3bca6a106e9af267935b93ac3bf7
KenKunoo <kennyfong12@yahoo.com>
2024-01-01 08:21:28 -0800

    setup

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Home = () => {
+    return (
+        <main className = 'w-full h-full flex flex-col first-letter: content-center items-center relative text-white font-nuinto'>
+        <div className = 'w-screen h-screen bg-gray-300 fixed -z-10' />
+        <Logo />
+
+        <Outlet />
+        </main>
+    
+    )
+}
+
+export default Home
