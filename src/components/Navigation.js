7c518b5d60a67932f51db53451263826b9c402a1
KenKunoo <kennyfong12@yahoo.com>
2024-01-02 17:13:38 -0800

    navigation tab

---
+++ src/components/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <nav className = "w-[40%] mt-16 flex justify-around align-middle border border-cyan rounded-lg">
+        Navigation</nav>
+  )
+}
+
+export default Navigation
