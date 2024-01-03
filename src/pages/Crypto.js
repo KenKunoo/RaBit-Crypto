3b6dae24ba4ecebd1eb65698fe33087a80e185ad
KenKunoo <kennyfong12@yahoo.com>
2024-01-03 08:09:56 -0800

    navi

--- src/pages/Crypto.js
+++ src/pages/Crypto.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Crypto = () => {
-    return (
-        <div>Crypto</div>
-    )
+  return (
+    <section className = 'w-[80%] h-full flex flex-col mt-16 mb-24 relative'>
+        Crypto</section>
+  )
 }
 
 export default Crypto
