3f39d696bc8f9a3f1490372e6aa66dfbd09a1728
KenKunoo <kennyfong12@yahoo.com>
2024-01-06 22:22:29 -0800

    tables

--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -8,7 +8,7 @@ export const CryptoProvider = ({children}) => {
     //Get Data and convert to json and catch errors
     const getCryptoData = async () => {
         try{
-            const data = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en').then(res => res.json()).then(json => json);
+            const data = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en').then(res => res.json()).then(json => json);
 
             console.log(data);
             setCryptoData(data);
