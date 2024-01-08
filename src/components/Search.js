59dfc944e56426fe5f9ecd43a25357b4be25870f
KenKunoo <kennyfong12@yahoo.com>
2024-01-07 17:21:53 -0800

    filter

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <form>
+        <input type="text" name="search" />
+        <button type="submit"></button>
+    </form>
+  )
+}
+
+export default Search
