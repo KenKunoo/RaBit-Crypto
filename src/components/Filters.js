a4b1dd3686c1bd48155deb9fbd7040319020edf8
KenKunoo <kennyfong12@yahoo.com>
2025-02-18 20:16:31 -0800

    fix

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -21,7 +21,7 @@ const Filters = () => {
     let val = e.target.value
     setSortBy(val);
   }
- // return
+ // return d
   return (
     <div className = 'w-full h-12 border-2 border-gray-100 rounded-lg flex items-center justify-between relative'>
     <Search/>
